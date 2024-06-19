bc4e1818311418cf895fd23aca35f9165d97e378
Nadim Mahmud Nion <nadim15-1746@diu.edu.bd>
2024-06-19 21:24:45 +0600

    text added in the carousel

--- src/pages/HomePage/Banner/Banner.jsx
+++ src/pages/HomePage/Banner/Banner.jsx
@@ -8,15 +8,30 @@ import image3 from '../../../assets/compParts-headers/image3.jpg';
 const Banner = () => {
     return (
         <div>
-            <Carousel showArrows={true}>
-                <div>
+            <Carousel showArrows={true} className='relative'>
+                <div className='relative'>
                     <img src={image1} />
+                    <div className='absolute inset-0 flex flex-col justify-center items-start bg-black bg-opacity-50 p-4 pl-12 text-left text-white space-y-7'>
+                        <h2 className='md:text-6xl text-2xl font-bold md:w-1/2 w-full'>Discover the Latest in Computer Hardware</h2>
+                        <p>Explore our wide selection of cutting-edge computer components, designed to enhance your systems performance.</p>
+                        <button className="btn btn-primary">Explore</button>
+                    </div>
                 </div>
                 <div>
                     <img src={image2} />
+                    <div className='absolute inset-0 flex flex-col justify-center items-start bg-black bg-opacity-50 p-4 pl-12 text-left text-white space-y-7'>
+                        <h2 className='md:text-6xl text-2xl font-bold md:w-1/2 w-full'>Unmatched Quality and Performance</h2>
+                        <p>At CompParts Hub, we pride ourselves on offering only the highest quality parts.</p>
+                        <button className="btn btn-primary">Explore</button>
+                    </div>
                 </div>
                 <div>
                     <img src={image3} />
+                    <div className='absolute inset-0 flex flex-col justify-center items-start bg-black bg-opacity-50 p-4 pl-12 text-left text-white space-y-7'>
+                        <h2 className='md:text-6xl text-2xl font-bold md:w-1/2 w-full'>Your Trusted Source for Computer Parts</h2>
+                        <p>With years of experience in the industry, CompParts Hub is your reliable partner for all your computer hardware needs.</p>
+                        <button className="btn btn-primary">Explore</button>
+                    </div>
                 </div>
             </Carousel>
         </div>
