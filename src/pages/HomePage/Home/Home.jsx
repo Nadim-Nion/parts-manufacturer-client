a994662f480efcf2754dfd22b8fbc18566907efe
Nadim Mahmud Nion <nadim15-1746@diu.edu.bd>
2024-07-03 17:05:46 +0600

    react helmet for dynamic title added

--- src/pages/HomePage/Home/Home.jsx
+++ src/pages/HomePage/Home/Home.jsx
@@ -5,10 +5,14 @@ import BusinessSummary from '../BusinessSummary/BusinessSummary';
 import Reviews from '../Reviews/Reviews';
 import TechNews from '../TechNews/TechNews';
 import BuildGuide from '../BuildGuide/BuildGuide';
+import { Helmet } from 'react-helmet-async';
 
 const Home = () => {
     return (
         <div>
+            <Helmet>
+                <title>Home - CompParts Hub</title>
+            </Helmet>
             <Banner></Banner>
             <BuildGuide></BuildGuide>
             <Parts></Parts>
--- src/pages/PurchasePage/Purchase/Purchase.jsx
+++ src/pages/PurchasePage/Purchase/Purchase.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import { Helmet } from 'react-helmet-async';
 
 const Purchase = () => {
     return (
         <div>
             <h2>This is Purchase Page</h2>
+            <Helmet>
+                <title>Purchase - CompParts Hub</title>
+            </Helmet>
         </div>
     );
 };
