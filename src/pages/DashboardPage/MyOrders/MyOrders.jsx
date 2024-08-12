769099e568aa66dca79b73b3b00d4bfb26a9b892
Nadim Mahmud Nion <nadim15-1746@diu.edu.bd>
2024-08-12 13:16:01 +0600

    payment implemented using stripe and display transaction id to the MyOrders.jsx

--- src/pages/DashboardPage/MyOrders/MyOrders.jsx
+++ src/pages/DashboardPage/MyOrders/MyOrders.jsx
@@ -1,13 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Swal from 'sweetalert2';
 import useAxiosSecure from '../../../hooks/useAxiosSecure';
 import { Link } from 'react-router-dom';
 import usePurchasedPartsDetails from '../../../hooks/usePurchasedPartsDetails';
+import useAuth from '../../../hooks/useAuth';
 
 const MyOrders = () => {
 
     const [purchasedPartsDetails, isLoading, refetch] = usePurchasedPartsDetails();
+    const [payments, setPayments] = useState([]);
     const axiosSecure = useAxiosSecure();
+    const { user } = useAuth();
+
+    // Fetch the Payment API to get the Transaction ID
+    useEffect(() => {
+        axiosSecure.get(`/payments?email=${user?.email}`)
+            .then(res => {
+                console.log(res.data);
+                setPayments(res.data);
+            })
+            .catch(error => {
+                console.log(error);
+            })
+    }, [axiosSecure, user?.email]);
 
     if (isLoading) {
         return <progress className="progress w-56"></progress>
@@ -80,35 +95,45 @@ const MyOrders = () => {
                     <tbody className='text-lg'>
                         {/* row 1 */}
                         {
-                            purchasedPartsDetails.map((item, index) => <tr key={item._id}>
-                                <th>{index + 1}</th>
-                                <td>{item.partsName}</td>
-                                <td>{item.userName}</td>
-                                <td>{item.userAddress}</td>
-                                <td>{item.phone}</td>
-                                <td>{item.quantity}</td>
-                                <td>
-                                    <Link to={`/dashboard/payment/${item._id}`} state={{ selectedItem: item }}>
-                                        <button className="btn btn-outline btn-primary">Pay</button>
-                                    </Link>
-                                </td>
-                                <td>
-                                    <button onClick={() => handleDelete(item)} className="btn btn-square btn-outline">
-                                        <svg
-                                            xmlns="http://www.w3.org/2000/svg"
-                                            className="h-6 w-6"
-                                            fill="none"
-                                            viewBox="0 0 24 24"
-                                            stroke="currentColor">
-                                            <path
-                                                strokeLinecap="round"
-                                                strokeLinejoin="round"
-                                                strokeWidth="2"
-                                                d="M6 18L18 6M6 6l12 12" />
-                                        </svg>
-                                    </button>
-                                </td>
-                            </tr>)
+                            purchasedPartsDetails.map((item, index) => {
+                                const payment = payments.find(payment => payment.productId === item._id);
+                                return (
+                                    <tr key={item._id}>
+                                        <th>{index + 1}</th>
+                                        <td>{item.partsName}</td>
+                                        <td>{item.userName}</td>
+                                        <td>{item.userAddress}</td>
+                                        <td>{item.phone}</td>
+                                        <td>{item.quantity}</td>
+                                        <td>
+                                            {
+                                                payment ? <p>{payment?.transactionId}</p>
+                                                    :
+                                                    <Link to={`/dashboard/payment/${item._id}`} state={{ selectedItem: item }}>
+                                                        <button className="btn btn-outline btn-primary">Pay</button>
+                                                    </Link>
+                                            }
+                                        </td>
+                                        <td>
+                                            <button disabled={payment?.transactionId} onClick={() => handleDelete(item)} className="btn btn-square btn-outline">
+                                                <svg
+                                                    xmlns="http://www.w3.org/2000/svg"
+                                                    className="h-6 w-6"
+                                                    fill="none"
+                                                    viewBox="0 0 24 24"
+                                                    stroke="currentColor">
+                                                    <path
+                                                        strokeLinecap="round"
+                                                        strokeLinejoin="round"
+                                                        strokeWidth="2"
+                                                        d="M6 18L18 6M6 6l12 12" />
+                                                </svg>
+                                            </button>
+                                        </td>
+                                    </tr>
+                                )
+                            }
+                            )
                         }
                     </tbody>
                 </table>
--- src/pages/DashboardPage/Payment/CheckoutForm.jsx
+++ src/pages/DashboardPage/Payment/CheckoutForm.jsx
@@ -2,14 +2,18 @@ import { CardElement, useElements, useStripe } from '@stripe/react-stripe-js';
 import React, { useEffect, useState } from 'react';
 import Swal from 'sweetalert2';
 import useAxiosSecure from '../../../hooks/useAxiosSecure';
+import useAuth from '../../../hooks/useAuth';
+import moment from 'moment';
 
 const CheckoutForm = ({ selectedItem }) => {
     const [clientSecret, setClientSecret] = useState("");
+    const { user } = useAuth();
     const stripe = useStripe();
     const elements = useElements();
     // console.log(selectedItem);
     const { totalPrice } = selectedItem;
     const axiosSecure = useAxiosSecure();
+
 
     useEffect(() => {
         axiosSecure.post('/create-payment-intent', { price: totalPrice })
@@ -18,6 +22,7 @@ const CheckoutForm = ({ selectedItem }) => {
                 setClientSecret(res.data.clientSecret);
             });
     }, [axiosSecure, totalPrice]);
+
 
     const handleSubmit = async (event) => {
         event.preventDefault();
@@ -32,13 +37,14 @@ const CheckoutForm = ({ selectedItem }) => {
             return;
         }
 
+        // Create a Payment Method
         const { error, paymentMethod } = await stripe.createPaymentMethod({
             type: 'card',
             card
         });
 
         if (error) {
-            console.log(error);
+            console.log('PaymentMethod Error', error);
 
             Swal.fire({
                 icon: "error",
@@ -48,7 +54,7 @@ const CheckoutForm = ({ selectedItem }) => {
             });
         }
         else {
-            console.log(paymentMethod);
+            console.log('Payment Method', paymentMethod);
 
             Swal.fire({
                 position: "top-end",
@@ -58,7 +64,59 @@ const CheckoutForm = ({ selectedItem }) => {
                 timer: 1500
             });
         }
+
+        // Confirm Payment
+        const { paymentIntent, error: confirmError } = await stripe.confirmCardPayment(clientSecret, {
+            payment_method: {
+                card: card,
+                billing_details: {
+                    email: user?.email || 'anonymous',
+                    name: user?.displayName || 'anonymous'
+                }
+            }
+        });
+
+        if (confirmError) {
+            console.log('Confirm Error', confirmError);
+
+            /*  Swal.fire({
+                 icon: "error",
+                 title: "Oops...",
+                 text: `${confirmError.message}`,
+                 footer: '<a href="#">Why do I have this issue?</a>'
+             }); */
+        }
+        else {
+            console.log('Payment Intent', paymentIntent);
+
+            if (paymentIntent.status === "succeeded") {
+                Swal.fire({
+                    position: "top-end",
+                    icon: "success",
+                    title: `Your Transaction ID: ${paymentIntent.id}`,
+                    showConfirmButton: false,
+                    timer: 1500
+                });
+
+                // Save the Payment Info to the database
+                const payment = {
+                    email: user?.email,
+                    name: user?.displayName,
+                    price: totalPrice,
+                    transactionId: paymentIntent.id,
+                    // date: new Date(),
+                    date: moment().format("dddd, MMMM D, YYYY, h:mm:ss a"),
+                    productName: selectedItem.partsName,
+                    productId: selectedItem._id,
+                    status: 'pending'
+                };
+
+                const res = await axiosSecure.post('/payments', payment);
+                console.log(res.data);
+            }
+        }
     };
+
 
     return (
         <form onSubmit={handleSubmit}>
